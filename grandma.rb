c5a26c5e16edb474e10fd7d3d04395670ff738e0
Hal H Nader <halnader89@gmail.com>
2020-06-22 15:32:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase
+  if phrase=="I LOVE YOU GRANDMA"
   "I LOVE YOU TOO PUMPKIN!"
   if phrase!=phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
