9a67482c4fa443f7159300c133c7340ca8ffa04d
Hal H Nader <halnader89@gmail.com>
2020-06-22 15:11:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(phrase)
   if phrase!=phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else 
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
